e3bd6304e6b99b9de2c9391b44b82f54544c03d8
Taxa8888 <Taxa8888@bk.ru>
2021-08-16 21:25:42 +0300

    feat: name all folders with lower case

--- src/Components/Button/button.tsx
+++ src/Components/Button/button.tsx
@@ -1,6 +1,6 @@
 import React, { FC, ReactElement } from 'react';
-import { ButtonProps } from './button.types';
 import './button.style.scss';
+import { ButtonProps } from './button.types';
 
 export const Button: FC<ButtonProps> = ({ className, onClick, style, children }): ReactElement => (
     <button className={className} type="button" onClick={onClick} style={style}>
